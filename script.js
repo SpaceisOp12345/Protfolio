193dd9a24957080e2dd991e56fe480df5df6015d
SpaceisOp <robloxdevexteam0@gmail.com>
2026-01-05 15:12:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,34 +11,6 @@ const hiddenElements = document.querySelectorAll('.hidden, .media-card, .section
 hiddenElements.forEach((el) => observer.observe(el));
 
 
-/* --- 3D TILT EFFECT (HERO CARD) --- */
-const card = document.querySelector('.glass-card');
-const container = document.querySelector('.hero-section');
-
-// Moving Animation Event
-container.addEventListener('mousemove', (e) => {
-    let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-    let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-    
-    // Limits rotation
-    if(xAxis > 20) xAxis = 20; if(xAxis < -20) xAxis = -20;
-    if(yAxis > 20) yAxis = 20; if(yAxis < -20) yAxis = -20;
-
-    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-});
-
-// Animate In (Reset)
-container.addEventListener('mouseenter', (e) => {
-    card.style.transition = 'none';
-});
-
-// Animate Out (Reset)
-container.addEventListener('mouseleave', (e) => {
-    card.style.transition = 'all 0.5s ease';
-    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-});
-
-
 /* --- TYPEWRITER EFFECT --- */
 const textToType = "Hii, My name is SpaceisOp. I like to code, play games, and build cool stuff on the internet.";
 const typewriterElement = document.getElementById("typewriter-text");
